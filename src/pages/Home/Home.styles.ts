4a295509e46ca12b45ce2c7f3a1357b5f86c7155
samuelsouzzza <=>
2023-12-26 12:45:00 -0300

    Ajuste

--- src/pages/Home/Home.styles.ts
+++ src/pages/Home/Home.styles.ts
@@ -7,7 +7,6 @@ export const Container = styled.section`
   background-repeat: no-repeat;
   width: 100%;
   height: 100vh;
-  background-color: #e9e9e9;
   display: flex;
   align-items: center;
   justify-content: center;
